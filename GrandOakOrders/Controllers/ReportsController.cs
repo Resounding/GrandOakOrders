7763be7532f695a5b7cff9bb9f17f6ceaf0d0dde
Cliffe Hodgkinson <cliffe@resounding.ca>
2015-09-07 11:13:49 -0400

    Error handling for report

--- GrandOakOrders/Controllers/ReportsController.cs
+++ GrandOakOrders/Controllers/ReportsController.cs
@@ -1,10 +1,9 @@
-﻿using System.IO;
+﻿using System;
 using System.Threading.Tasks;
 using System.Web.Http;
 using GrandOakOrders.Data.Repositories;
 using GrandOakOrders.Reports;
 using GrapeCity.ActiveReports;
-using GrapeCity.ActiveReports.Export.Pdf.Section;
 
 namespace GrandOakOrders.Controllers
 {
@@ -15,15 +14,19 @@ namespace GrandOakOrders.Controllers
         [HttpGet]
         public async Task<IHttpActionResult> KitchenOrder(int id)
         {
-            var repo = new OrderRepository();
-            var order = await repo.Get(id);
-            if(order == null) {
-                return NotFound();
-            }
+            try {
+                var repo = new OrderRepository();
+                var order = await repo.Get(id);
+                if (order == null) {
+                    return NotFound();
+                }
 
-            SectionReport report = new KitchenOrderReport(order) as SectionReport;
-            report.Run();
-            return Ok(report);
+                SectionReport report = new KitchenOrderReport(order) as SectionReport;
+                report.Run();
+                return Ok(report);
+            } catch (Exception ex) {
+                return Ok(ex);
+            }
         }
     }
 }
